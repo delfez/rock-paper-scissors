af29d0883254539309fd1c13331ec4e4e66caa54
delfez <delfez37@protonmail.com>
2023-09-29 16:50:29 -0300

    create function that plays one round of the game and declares winner

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
-console.log("Hello World!")
+
 
 let num = Math.floor(Math.random()* 3) + 1
 
 function getComputerChoice(){
 
    if (num === 1){
-    return "rock"
+    return "ROCK"
    } else if (num === 2) {
-    return "paper"
+    return "PAPER"
    } else {
-    return "scissors"
+    return "SCISSORS"
    }
-
-  
+ 
 }
+
+function playRound(playerSelection, computerSelection) {
+
+    if ((playerSelection === "ROCK" && computerSelection === "ROCK") || 
+    ((playerSelection === "PAPER" && computerSelection === "PAPER")) || 
+    ((playerSelection === "SCISSORS" && computerSelection === "SCISSORS"))) {
+
+        return "Tie"
+        
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER"){
+        return "You Lose!"
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
+        return "You Win!"
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS"){
+        return "You Lose!"
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK"){
+        return "You Win!"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
+        return "You Win!"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK"){
+        return "You Lose!"
+    }      
+
+}
+
+function game(){
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+let computerSelection = getComputerChoice()
+let playerSelection = (prompt("Rock, Paper or Scissors?")).toUpperCase()
+
+
+console.log(playRound(playerSelection, computerSelection))
 
 console.log(getComputerChoice())
