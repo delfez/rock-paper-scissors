06b6b242df0221da766f279cdccd3acc749bbc7d
delfez <delfez37@protonmail.com>
2023-10-04 02:25:31 -0300

    edit playRound function and create game function

--- script.js
+++ script.js
@@ -14,37 +14,61 @@ function getComputerChoice(){
 
 
 function playRound(playerSelection, computerSelection) {
-
+    
     if (playerSelection === computerSelection) {
 
         return "Tie"
         
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER"){
-        return "You Lose!"
+        computerPoints++
+        return "You Lose! Paper covers Rock."
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
-        return "You Win!"
+        playerPoints++
+        return "You Win! Rock crushes Scissors."
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS"){
-        return "You Lose!"
+        computerPoints++
+        return "You Lose! Scissors cuts Paper."
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK"){
-        return "You Win!"
+        playerPoints++
+        return "You Win! Paper covers Rock."
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-        return "You Win!"
+        playerPoints++
+        return "You Win! Scissors cut Paper."
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK"){
-        return "You Lose!"
+        computerPoints++
+        return "You Lose! Rock crushes Scissors."
+    } else{
+       
+        return"You must choose between rock, paper or scissors."  
+
+       
     }      
 
 }
+    let computerPoints = 0
+    let playerPoints = 0
+   
 
 function game(){
     
     
     for (let i = 0; i < 5; i++ ){
     let computerSelection = getComputerChoice()
-    let playerSelection = prompt("").toUpperCase()
+    let playerSelection = prompt("Choose your weapon!").toUpperCase()
     console.log(playRound(playerSelection, computerSelection))
+    console.log("Player Points: " + playerPoints + " Computer Points: " + computerPoints)
     }
 
+    if (playerPoints > computerPoints){
+        console.log("Game Over! Congratulations, you won!")
+    } else if (playerPoints < computerPoints){
+        console.log("Game Over! You lost. Unlucky.")
+    } else {
+        console.log("Game Over! It's a tie!")
+    }
 }
+
+
 
 
 
